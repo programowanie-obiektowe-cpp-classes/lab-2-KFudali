e8854d05b726633b2323976e56badd69e48633dc
Krystian Fudali <krystian.fudali42@gmail.com>
2022-11-02 00:48:07 +0100

    added all constructors

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,19 +5,33 @@
 class ResourceManager
 {
         private:
-            Resource resource;
+            Resource* resource;
         public:
         ResourceManager()
         {
-
+            Resource res;
+            resource = &res;
+        }
+        double get()
+        {
+            return resource->get();
         }
         ResourceManager(const ResourceManager& rm):resource{rm.resource}
         {
 
         }
-        double get()
+        ResourceManager& operator=(const ResourceManager& movedManager)
         {
-            return resource.get();
+            return *this;
         }
-
+        ResourceManager(ResourceManager&& sourceManager)
+        {
+            ResourceManager movedManager;
+            movedManager.resource = sourceManager.resource;
+            sourceManager.resource = nullptr;
+        }
+        ~ResourceManager()
+        {
+            delete resource;
+        }
 };
